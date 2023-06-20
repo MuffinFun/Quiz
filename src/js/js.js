03bb116200fa057233cd634d41e69acdcd1065de
MaksimKriv <max.kriv3228@gmail.com>
2023-06-20 23:38:40 +0300

    styling and adding alittle javasctips

---
+++ src/js/js.js
@@ -0,0 +1,20 @@
+const startBtn = document.querySelector(".start-button");
+const startMenu = document.querySelector(".start-menu");
+const contBtn = document.querySelector(".continue-button");
+const exitBtn = document.querySelector(".exit-button");
+
+startBtn.addEventListener("click", () => {
+  startBtn.style.opacity = "0";
+  startMenu.style.transform = "translate(-50%, -50%)";
+});
+
+contBtn.addEventListener("click", () => {
+  startMenu.style.transition = "1s";
+  startMenu.style.transform = "translate(-500%, -50%)";
+});
+
+exitBtn.addEventListener("click", () => {
+  startBtn.style.opacity = "1";
+  startBtn.style.transition = "550ms";
+  startMenu.style.transform = "translate(-50%, -300%)";
+});

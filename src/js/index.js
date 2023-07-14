ae33e2639240b9e52d487c39e8abee8921663a83
MaksimKriv <max.kriv3228@gmail.com>
2023-07-14 12:06:25 +0300

    chore(index.js) move to end and style(style.css)

--- src/js/index.js
+++ src/js/index.js
@@ -16,12 +16,21 @@ const concedeBtn = document.querySelector(".concede-button");
 const exitBtn = document.querySelector(".exit-button");
 const okBtn = document.querySelector(".ok-button");
 
+let answerButton;
+let timer;
+
+let correctAns;
+let corrSevAns;
+let currentQuestion;
+
+let count = 0;
+let index = 0;
+
 const inputViolet = document
   .querySelector("#violet")
   .addEventListener("click", () => {
     changeTheme(0);
   });
-
 const inputGreen = document
   .querySelector("#green")
   .addEventListener("click", () => {
@@ -33,15 +42,6 @@ const inputDark = document
     changeTheme(2);
   });
 
-let answerButton;
-let timer;
-
-let correctAns;
-let corrSevAns;
-let currentQuestion;
-
-let count = 0;
-let index = 0;
 
 startBtn.addEventListener("click", () => {
   classManagment("startBtn");
@@ -224,64 +224,6 @@ function changeTheme(themeNumber) {
   okBtn.style.background = listOfThemes[themeNumber].contConcedeExitOkBtn;
 }
 
-const listOfThemes = [
-  {
-    container: "rgb(232, 138, 234)",
-    startEndMenuNoteBox: `linear-gradient(
-      316deg,
-      rgba(36, 5, 38, 1) 0%,
-      rgba(82, 9, 91, 1) 16%,
-      rgba(138, 14, 157, 1) 79%,
-      rgba(163, 18, 185, 1) 90%
-    )`,
-    startButton: `linear-gradient(
-      74deg,
-      rgba(187, 18, 193, 1) 9%,
-      rgba(187, 18, 193, 1) 46%,
-      rgba(103, 10, 122, 1) 77%,
-      rgba(65, 6, 90, 1) 87%,
-      rgba(0, 12, 36, 1) 99%
-    )`,
-    contConcedeExitOkBtn: `linear-gradient(
-      316deg,
-      rgba(77, 10, 66, 1) 0%,
-      rgba(120, 14, 93, 1) 1%,
-      rgba(190, 16, 118, 1) 40%,
-      rgba(221, 18, 138, 1) 90%
-    )`,
-  },
-  {
-    container: "rgb(77, 201, 77)",
-    startEndMenuNoteBox: `linear-gradient(
-      316deg,
-      rgba(3, 57, 6, 1) 0%,
-      rgba(8, 96, 29, 1) 16%,
-      rgba(5, 153, 65, 1) 79%,
-      rgba(9, 179, 78, 1) 90%
-    )`,
-    startButton: `linear-gradient(
-      74deg,
-      rgba(6, 194, 100, 1) 9%,
-      rgba(22, 143, 70, 1) 46%,
-      rgba(27, 129, 68, 1) 63%,
-      rgba(13, 95, 31, 1) 87%,
-      rgba(7, 69, 10, 1) 100%
-    )`,
-    contConcedeExitOkBtn: `linear-gradient(
-      316deg,
-      rgba(3, 57, 6, 1) 0%,
-      rgba(8, 96, 29, 1) 1%,
-      rgba(5, 153, 65, 1) 40%,
-      rgba(9, 179, 78, 1) 90%
-    )`,
-  },
-  {
-    container: "rgb(18, 18, 18)",
-    startEndMenuNoteBox: `rgb(26, 24, 24)`,
-    startButton: `rgb(26, 24, 24)`,
-    contConcedeExitOkBtn: `rgb(18, 18, 18)`,
-  },
-];
 
 function classManagment(element, moment) {
   switch (element) {
@@ -409,6 +351,65 @@ const listOfQuestions = [
     question: "How many primitive data types in JavaScript",
     correctAnswer: ["8"],
     answers: ["4", "10", "8", "5"],
+  },
+];
+
+const listOfThemes = [
+  {
+    container: "rgb(232, 138, 234)",
+    startEndMenuNoteBox: `linear-gradient(
+      316deg,
+      rgba(36, 5, 38, 1) 0%,
+      rgba(82, 9, 91, 1) 16%,
+      rgba(138, 14, 157, 1) 79%,
+      rgba(163, 18, 185, 1) 90%
+    )`,
+    startButton: `linear-gradient(
+      74deg,
+      rgba(187, 18, 193, 1) 9%,
+      rgba(187, 18, 193, 1) 46%,
+      rgba(103, 10, 122, 1) 77%,
+      rgba(65, 6, 90, 1) 87%,
+      rgba(0, 12, 36, 1) 99%
+    )`,
+    contConcedeExitOkBtn: `linear-gradient(
+      316deg,
+      rgba(77, 10, 66, 1) 0%,
+      rgba(120, 14, 93, 1) 1%,
+      rgba(190, 16, 118, 1) 40%,
+      rgba(221, 18, 138, 1) 90%
+    )`,
+  },
+  {
+    container: "rgb(77, 201, 77)",
+    startEndMenuNoteBox: `linear-gradient(
+      316deg,
+      rgba(3, 57, 6, 1) 0%,
+      rgba(8, 96, 29, 1) 16%,
+      rgba(5, 153, 65, 1) 79%,
+      rgba(9, 179, 78, 1) 90%
+    )`,
+    startButton: `linear-gradient(
+      74deg,
+      rgba(6, 194, 100, 1) 9%,
+      rgba(22, 143, 70, 1) 46%,
+      rgba(27, 129, 68, 1) 63%,
+      rgba(13, 95, 31, 1) 87%,
+      rgba(7, 69, 10, 1) 100%
+    )`,
+    contConcedeExitOkBtn: `linear-gradient(
+      316deg,
+      rgba(3, 57, 6, 1) 0%,
+      rgba(8, 96, 29, 1) 1%,
+      rgba(5, 153, 65, 1) 40%,
+      rgba(9, 179, 78, 1) 90%
+    )`,
+  },
+  {
+    container: "rgb(18, 18, 18)",
+    startEndMenuNoteBox: `rgb(26, 24, 24)`,
+    startButton: `rgb(26, 24, 24)`,
+    contConcedeExitOkBtn: `rgb(18, 18, 18)`,
   },
 ];
 
